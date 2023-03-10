869f4ee01fcbf8cfc619d09c35ef5b0a522dcd7b
Daniel Reintanz <85614916+FreelancerAMP@users.noreply.github.com>
2023-03-10 14:14:43 +0100

    CHANGES: REFACTORED viewer.js

--- js/viewer.js
+++ js/viewer.js
@@ -1,8 +1,8 @@
 jQuery(document).ready($ => {
     $('.acf-form').append('<iframe id="viewer" style="height:45vh; width: 100%"  src="https://ktwu.rpi-virtuell.de/wp-content/ktwu-iframe/viewer.php"> </iframe>')
     $('.acf-radio-list input').on('change', function (e) {
-        var iframe = document.getElementById('viewer');
-        var innerDoc = iframe.contentDocument;
+        const iframe = document.getElementById('viewer');
+        const innerDoc = iframe.contentDocument;
         for (const enti of innerDoc.querySelectorAll('a-entity[text]')) {
 
             enti.components.text.data.font = 'https://ktwu.rpi-virtuell.de/wp-content/ktwu-iframe/assets/' + $(e.target).val() + '.json'
@@ -10,27 +10,23 @@ jQuery(document).ready($ => {
         }
     })
     $('.acf-field-textarea textarea').on('change', function (e) {
-        var iframe = document.getElementById('viewer');
-        var innerDoc = iframe.contentDocument;
+        const iframe = document.getElementById('viewer');
+        const innerDoc = iframe.contentDocument;
         for (const enti of innerDoc.querySelectorAll('a-entity[text]')) {
-
             enti.components.text.data.value = $(e.target).val();
             enti.components.text.updateProperties();
         }
     })
     $('.acf-color-picker input').on('change', function (e) {
-        var iframe = document.getElementById('viewer');
-        var innerDoc = iframe.contentDocument;
-        for (const enti of innerDoc.querySelectorAll('a-entity[text]')) {
-
-            enti.components.text.data.color = $(e.target).val();
-            enti.components.text.updateProperties();
-            break;
-        }
+        const iframe = document.getElementById('viewer');
+        const innerDoc = iframe.contentDocument;
+        let fronttextnode = innerDoc.querySelector('a-entity[fronttext]');
+        fronttextnode.components.text.data.color = $(e.target).val();
+        fronttextnode.components.text.updateProperties();
     })
     $('.acf-field-number input').on('change', function (e) {
-        var iframe = document.getElementById('viewer');
-        var innerDoc = iframe.contentDocument;
+        const iframe = document.getElementById('viewer');
+        const innerDoc = iframe.contentDocument;
         for (const enti of innerDoc.querySelectorAll('a-entity[text]')) {
             var scale = $(e.target).val();
             enti.setAttribute('scale', scale + ' ' + scale + ' ' + scale);
