34328f0f0c29cbe787ef647b73865c6eee7148d3
Daniel Reintanz <85614916+FreelancerAMP@users.noreply.github.com>
2023-03-10 16:30:14 +0100

    CHANGES: ALTERED code to prepend ar viewer in viewer.js

--- js/viewer.js
+++ js/viewer.js
@@ -1,5 +1,5 @@
 jQuery(document).ready($ => {
-    $('.acf-form').append('<iframe id="viewer" style="height:45vh; width: 100%"  src="https://ktwu.rpi-virtuell.de/wp-content/ktwu-iframe/viewer.php"> </iframe>')
+    $('.acf-form').prepend('<iframe id="viewer" style="height:45vh; width: 100%"  src="https://ktwu.rpi-virtuell.de/wp-content/ktwu-iframe/viewer.php"> </iframe>')
     $('.acf-radio-list input').on('change', function (e) {
         const iframe = document.getElementById('viewer');
         const innerDoc = iframe.contentDocument;
