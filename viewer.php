0f61a3bb35ef895db83c458f480f681a09fb8245
Joachim Happel <joachim.happel@gmail.com>
2023-03-14 09:13:03 +0100

    changed positions

--- viewer.php
+++ viewer.php
@@ -8,13 +8,13 @@ $base_url = str_replace('/viewer.php', '', 'https://' . $_SERVER['SERVER_NAME'] 
     <script src="https://cdn.jsdelivr.net/gh/aframevr/aframe@master/dist/aframe-master.min.js"></script>
 </head>
 <body>
-<a-scene debug="true" background="color:transparent;">
+<a-scene debug="true" background="color:gray;">
     <a-entity light="type:directional; castShadow:true;" position="1 1 5"></a-entity>
 
     <a-entity
             messagetext front
-            text="font:<?php echo $base_url; ?>/assets/PermanentMarker-Regular-msdf.json; value: Klappentext;negate:false; align:center; shader:msdf; color:#fff; width:0.3;opacity:0.5 ; side:double; wrapPixels:450 ; baseline:bottom"
-            position="0 1.45 -0.245" scale="1 1 1" rotation="0 0 0"
+            text="font:<?php echo $base_url; ?>/assets/PermanentMarker-Regular-msdf.json; value: Klappentext;negate:false; align:center; shader:msdf; color:#fff; width:0.3;opacity:0.9 ; side:double; wrapPixels:450 ; baseline:bottom"
+            position="0 1.50 -0.245" scale="1 1 1" rotation="0 0 0"
             animation="property: rotation; to: -10 10 0; dur: 2000; easing: easeInOutQuad; loop: true; dir: alternate"
             material="side:double"
 
@@ -23,7 +23,7 @@ $base_url = str_replace('/viewer.php', '', 'https://' . $_SERVER['SERVER_NAME'] 
     <a-entity
             messagetext back
             text="font:<?php echo $base_url; ?>/assets/PermanentMarker-Regular-msdf.json; value: Klappentext;negate:false; align:center; shader:msdf; color:#000; width:0.3;opacity:0.15 ; side:double; wrapPixels:450 ; baseline:bottom"
-            position="0.01 1.45 -0.25" scale="1 1 1" rotation="0 0 0"
+            position="0.01 1.50 -0.25" scale="1 1 1" rotation="0 0 0"
             animation="property: rotation; to: -10 10 0; dur: 2000; easing: easeInOutQuad; loop: true; dir: alternate"
             material="side:double"
 
@@ -31,7 +31,7 @@ $base_url = str_replace('/viewer.php', '', 'https://' . $_SERVER['SERVER_NAME'] 
 
         <a-entity
                 authortext front
-                text="font:<?php echo $base_url; ?>/assets/PermanentMarker-Regular-msdf.json; value: author;negate:false; align:right; shader:msdf; color:#fff; opacity:0.5 ; side:double; wrapPixels:400"
+                text="font:<?php echo $base_url; ?>/assets/PermanentMarker-Regular-msdf.json; value: author;negate:false; align:right; shader:msdf; color:#fff; opacity:0.9 ; side:double; wrapPixels:400"
                 position="0 -0.02 0" scale="0.2 0.2 0.2" rotation="0 0 0"
                 material="side:double"
 
@@ -44,7 +44,7 @@ $base_url = str_replace('/viewer.php', '', 'https://' . $_SERVER['SERVER_NAME'] 
 
         ></a-entity>
     </a-entity>
-        <a-image src="<?php echo $base_url; ?>/assets/background.jpg" position="0.01 0.8 -1.25" scale="5 3 0" rotation="-20 0 0">
+        <a-image src="<?php echo $base_url; ?>/assets/background.jpg" position="-0.2 2 -3.25" scale="5 3 4" width="2" height="2"  rotation="0 0 0">
     </a-image>
 
 </a-scene>
